0cb992fda44cc8f9c3367b3062e2cfdb430bd3c5
Rachael Oxley <rachaelnaeva@gmail.com>
2025-02-20 18:01:01 +1100

    add movement direction

--- snake.gd
+++ snake.gd
@@ -14,6 +14,7 @@ const SNAKE_BODYPARTS := [
 
 var move_seconds := 1.0
 var movement_direction := Vector2.RIGHT
+var next_movement_direction := Vector2.RIGHT
 
 var grid_size := 32
 
@@ -30,6 +31,22 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func _unhandled_input(event: InputEvent) -> void:
+	if movement_direction == Vector2.RIGHT or movement_direction == Vector2.LEFT:
+		if event.is_action_pressed("move_up"):
+			set_next_movement_direction(Vector2.UP)
+		if event.is_action_pressed("move_down"):
+			set_next_movement_direction(Vector2.DOWN)
+	if movement_direction == Vector2.UP or movement_direction == Vector2.DOWN:
+		if event.is_action_pressed("move_left"):
+			set_next_movement_direction(Vector2.LEFT)
+		if event.is_action_pressed("move_right"):
+			set_next_movement_direction(Vector2.RIGHT)
+
+func set_next_movement_direction(direction: Vector2) -> void:
+	next_movement_direction = direction
+	# Point the head moving
+	head.rotation = next_movement_direction.angle()
 
 func move() -> void:
 	# Initialising the snake body
@@ -38,6 +55,7 @@ func move() -> void:
 		new_part.get_node("Sprite2D").texture = SNAKE_BODYPARTS[snake_length]
 		snake_body.get_child(snake_length - 1).add_sibling(new_part)
 		snake_length += 1
+	movement_direction = next_movement_direction
 	var next_position = head.position + grid_size * movement_direction
 	for body_part: Area2D in snake_body.get_children():
 		var current_position := body_part.position
